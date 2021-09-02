c5502fd7a85be127730cd0fb5d2c2eff8753c71f
张威 <zhangwei@Mac-mini.local>
2021-09-02 13:49:25 +0800

    添加了view

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,31 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+  <Row>
+    <Col span="12">col-12</Col>
+    <Col span="12">col-12</Col>
+  </Row>
+  <br>
+  <Row>
+    <Col span="8">col-8</Col>
+    <Col span="8">col-8</Col>
+    <Col span="8">col-8</Col>
+  </Row>
+  <br>
+  <Row>
+    <Col span="6">col-6</Col>
+    <Col span="6">col-6</Col>
+    <Col span="6">col-6</Col>
+    <Col span="6">col-6</Col>
+  </Row>
   </div>
 </template>
+<script>
+export default {
+
+}
+</script>
+<style>
+Row{
+  background-color: #42b983;
+}
+</style>
